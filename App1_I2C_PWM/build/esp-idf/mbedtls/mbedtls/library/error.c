a5b21484a962eef4bc26db2ac4f9023bef0796ed
AlexOliZ <alexandretomas@ua.pt>
2022-04-13 18:49:26 +0100

    first commit uart and pwm

---
+++ App1_I2C_PWM/build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/zedleague/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
---
+++ App3_ADC_UART/main/adc1_example_main.c
@@ -0,0 +1,257 @@
+/* ADC1 Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "driver/uart.h"
+#include "esp_adc_cal.h"
+#include "sdkconfig.h"
+#include "esp_rom_gpio.h"
+#include "soc/uart_periph.h"
+#include "driver/gpio.h"
+
+
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   64          //Multisampling
+
+#define ECHO_UART_BAUD_RATE     115200
+#define BUF_SIZE (1024)
+
+#define UART1_GPIO_RX     (4)
+#define UART1_GPIO_TX     (5)
+
+#define TURN_OFF_GPIO     (36)
+
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+static const adc_atten_t atten = ADC_ATTEN_DB_0;
+static const adc_unit_t unit = ADC_UNIT_1;
+
+const char xOn[] = {0x11, '\0'};
+const char xOff[] = {0x13, '\0'};
+
+char* convertIntegerToChar(int N)
+{
+ 
+    int m = N;
+    int digit = 0;
+    while (m) {
+        digit++;
+        m /= 10;
+    }
+ 
+    char* arr;
+    char arr1[digit];
+    arr = (char*)malloc(digit);
+
+    int index = 0;
+    while (N) {
+        arr1[++index] = N % 10 + '0';
+        N /= 10;
+    }
+ 
+    int i;
+    for (i = 0; i < index; i++) {
+        arr[i] = arr1[index - i];
+    }
+    arr[i] = '\0';
+ 
+    return (char*)arr;
+}
+
+
+static void uart1_task(void *arg)
+{
+    //uint8_t pointer = 0;
+    uint32_t voltage;
+    while (1) {
+        uint8_t adc_reading = 0;
+        //Multisampling
+        for (int i = 0; i < NO_OF_SAMPLES; i++) {
+            adc_reading += adc1_get_raw((adc1_channel_t)channel);
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        //Convert adc_reading to voltage in mV
+        voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        //pointer = (pointer+1)%(BUF_SIZE/sizeof(uint8_t));
+        const char* buff_values = convertIntegerToChar(voltage);
+        printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+        uart_write_bytes(UART_NUM_1, buff_values, sizeof(buff_values));
+        //const char characters[] = "HELLO UART2";
+        //uart_write_bytes(UART_NUM_1, characters, sizeof(characters));
+        //sleep for 1000 ms
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void uart2_task(void *arg)
+{
+    unsigned int count = 0;
+    int lastGPIOValue = 0;
+    while (1) {
+        // Read data from the UART
+        uint8_t data[128];
+        int length = 0;
+        ESP_ERROR_CHECK(uart_get_buffered_data_len(UART_NUM_2, (size_t*)&length));
+        if(length != 0 ) {
+            uart_read_bytes(UART_NUM_2, data, length, 20 / portTICK_PERIOD_MS);
+            printf("%d - UART2 received from UART1: %s\n", count, (char*)data);
+            count++;
+        }
+        
+        // Check gpio level
+        int value = gpio_get_level(TURN_OFF_GPIO);
+        if(value == 1 && lastGPIOValue == 0)
+        {
+            uart_write_bytes(UART_NUM_2, &xOff, sizeof(xOff));
+            printf("UART2 : turn off transmission\n");
+        }
+        if(value == 0 && lastGPIOValue == 1)
+        {
+            uart_write_bytes(UART_NUM_2, &xOn, sizeof(xOn));
+            printf("UART2 : turn on transmission\n");
+        }
+        lastGPIOValue = value;
+
+        //sleep for 100 ms
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}
+
+
+static void check_efuse(void)
+{
+    //Check if TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using Default Vref\n");
+    }
+}
+
+
+
+
+void app_main(void)
+{
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = ECHO_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    int intr_alloc_flags = 0;
+    
+#if CONFIG_UART_ISR_IN_IRAM
+    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_1, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_2, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+    //enabling software control flow for UART1
+    uart_set_sw_flow_ctrl(UART_NUM_1, true, 0, 0);
+
+    /* Connect uart1 and uart 2 */
+    //UART2 TX -> UART1_GPIO_RX
+    esp_rom_gpio_connect_out_signal(UART1_GPIO_RX, UART_PERIPH_SIGNAL(2, SOC_UART_TX_PIN_IDX), false, false);
+    //UART1 RX -> UART1_PIN_RX
+    esp_rom_gpio_connect_in_signal(UART1_GPIO_RX, UART_PERIPH_SIGNAL(1, SOC_UART_RX_PIN_IDX), false);
+
+    //UART1 TX -> UART1_GPIO_TX
+    esp_rom_gpio_connect_out_signal(UART1_GPIO_TX, UART_PERIPH_SIGNAL(1, SOC_UART_TX_PIN_IDX), false, false);
+    //UART2 RX -> UART1_GPIO_TX
+    esp_rom_gpio_connect_in_signal(UART1_GPIO_TX, UART_PERIPH_SIGNAL(2, SOC_UART_RX_PIN_IDX), false);
+
+
+    //initialize the GPIO config structure.
+    gpio_config_t io_conf = {};
+    
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    
+    //set as output mode
+    io_conf.mode = GPIO_MODE_INPUT;
+
+    //bit mask of the pins that you want to set,e.g.GPIO36
+    io_conf.pin_bit_mask = 1ULL << TURN_OFF_GPIO;
+    
+    //enable pull-down mode
+    io_conf.pull_down_en = 1;
+    
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //Check if Two Point or Vref are burned into eFuse
+    check_efuse();
+
+    //Configure ADC
+    adc1_config_width(width);
+    adc1_config_channel_atten(channel, atten);
+
+    //Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
+    xTaskCreate(uart1_task, "uart1_task", 2048, NULL, 10, NULL);
+    xTaskCreate(uart2_task, "uart2_task", 2048, NULL, 10, NULL);
+
+    //Continuously sample ADC1
+    /*
+    while (1) {
+        uint32_t adc_reading = 0;
+        //Multisampling
+        for (int i = 0; i < NO_OF_SAMPLES; i++) {
+            adc_reading += adc1_get_raw((adc1_channel_t)channel);
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        //Convert adc_reading to voltage in mV
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    */
+}
